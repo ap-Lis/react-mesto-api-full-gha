f244e633d82667b577ed9d1cc0e6597e281663c5
Gleb <gleb@grib.in>
2023-08-26 21:34:48 +0300

    no limiter

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
-const rateLimit = require('express-rate-limit');
 const urlPattern = require('./utils/constants');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
@@ -20,10 +19,6 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 200,
-});
 
 app.use(helmet());
 
@@ -43,8 +38,6 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
-app.use(limiter);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
