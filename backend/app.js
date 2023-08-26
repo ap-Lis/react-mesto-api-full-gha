e2905edd4c123ba5a0c9802089be774a788a6338
Gleb <gleb@grib.in>
2023-08-26 21:22:34 +0300

    cors 5

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 200,
+  standardHeaders: true,
 });
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
