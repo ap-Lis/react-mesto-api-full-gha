cdb9e60c161fbb363a15ddf4cb07a227177b1d29
Gleb <gleb@grib.in>
2023-08-26 16:10:08 +0300

    cors

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.ap-mesto.nomoredomainsicu.ru';
 
 function getOkOrNot(res) {
     if (res.ok) {
@@ -12,7 +12,7 @@ export const register = ({password, email}) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json' 
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({password, email })
   }).then(getOkOrNot);
