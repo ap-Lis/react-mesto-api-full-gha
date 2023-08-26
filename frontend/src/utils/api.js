1fb7540adcab4a6865d32decb9e04fd6fbc38359
Gleb <gleb@grib.in>
2023-08-26 17:43:45 +0300

    https fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -118,6 +118,6 @@ class Api {
     }
 }
 
-const api = new Api({url: 'http://api.ap-mesto.nomoredomainsicu.ru/', token: localStorage.getItem('jwt')});
+const api = new Api({url: 'https://api.ap-mesto.nomoredomainsicu.ru/', token: localStorage.getItem('jwt')});
 
 export default api;
