644c95fe39ef8339de0d4940714c2468ab08326d
Gleb <gleb@grib.in>
2023-08-26 21:24:32 +0300

    cors 6

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ const limiter = rateLimit({
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+app.use(limiter);
 app.use(helmet());
 
 app.use(corsHandler);
@@ -38,8 +39,6 @@ mongoose.connect(DB_URL, {
 });
 
 app.use(requestLogger);
-
-app.use(limiter);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
