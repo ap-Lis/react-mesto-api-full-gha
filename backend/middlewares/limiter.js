c27bc292221c45f1e5780b601238b58af831ff0d
Gleb <gleb@grib.in>
2023-08-26 22:34:33 +0300

    limiter handler

--- backend/middlewares/limiter.js
+++ backend/middlewares/limiter.js
@@ -1,13 +1,21 @@
 const rateLimit = require('express-rate-limit');
 
-const limiterHandler = (req, res, next) => {
+const limiterHandler = () => {
   rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 200,
     headers: true,
     message: 'Слишком много запросов с этого IP, попоробуйте повторить попытку через 15 минут.',
+    handler(req, res, next) {
+      res.removeHeader('X-Powered-By');
+      res.setHeader('X-Content-Type-Options', 'nosniff');
+      res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+      res.setHeader('X-XSS-Protection', '1; mode=block');
+      res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+      res.status(this.statusCode || 429).send(this.message);
+      return next();
+    },
   });
-  return next();
 };
 
 module.exports = limiterHandler;
